7d3b5583427b3296e8ac1324b2fa4688ad20fb3b
Rafiur Rahman <rrfoundary@gmail.com>
2021-05-26 10:04:18 +0600

    handling single input form - start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
-import Palette from "./palette";
-import seedColors from "./seedColors";
-import { generatePalette } from "./utils/colorhelpers";
+import FormClass from "./formClass";
 class App extends Component {
   render() {
-    return <Palette palette={generatePalette(seedColors[4])} />;
+    return <FormClass />;
   }
 }
 
---
+++ src/formClass.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+class FormClass extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h1>Form</h1>
+      </div>
+    );
+  }
+}
+
+export default FormClass;
