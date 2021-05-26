a286fdf94f83648ff58953c8891a9d8b6cd56659
Rafiur Rahman <rrfoundary@gmail.com>
2021-05-26 10:12:56 +0600

    Handling single input form - complete

--- src/formClass.jsx
+++ src/formClass.jsx
@@ -2,12 +2,39 @@ import React, { Component } from "react";
 class FormClass extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { username: "", password: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.username, this.state.password);
+    this.setState({ username: "", password: "" });
   }
   render() {
     return (
       <div>
         <h1>Form</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="username"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            placeholder="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+          <button>Submit</button>
+        </form>
       </div>
     );
   }
