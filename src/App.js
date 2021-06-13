aec1dee9adecad5ccc58bde0cb5fe975730e2642
Rafiur Rahman <rrfoundary@gmail.com>
2021-06-13 10:14:26 +0600

    Adding routing playground

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
-import UnderstandingBreakpoint from "./99. Material-ui/understandingBreakpoints";
+import NavBar from "./routing-practise/components/navbar";
+import Products from "./routing-practise/components/products";
+import Posts from "./routing-practise/components/posts";
+import Home from "./routing-practise/components/home";
+import Dashboard from "./routing-practise/components/admin/dashboard";
+import ProductDetails from "./routing-practise/components/productDetails";
+import NotFound from "./routing-practise/components/notFound";
+// import "./App.css";
+
 class App extends Component {
   render() {
-    return <div>Hi</div>;
+    return (
+      <div>
+        <NavBar />
+      </div>
+    );
   }
 }
 
