f90bf12e8c88e07f952c082f336bd5ec8424c63b
Rafiur Rahman <rrfoundary@gmail.com>
2021-06-17 11:15:16 +0600

    Added isHappy functionality

--- src/100. todolist/toggler.jsx
+++ src/100. todolist/toggler.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 const Toggler = (props) => {
-  return <h1>😁</h1>;
+  const [isHappy, setIsHappy] = useState(false);
+  const [isMarried, setIsMarried] = useState(false);
+  const changeIsHappy = () => {
+    setIsHappy(!isHappy);
+  };
+  const changeIsMarried = () => {
+    setIsMarried(!isMarried);
+  };
+  return (
+    <div>
+      <h1 onClick={changeIsHappy}>{isHappy ? "😁" : "😒"}</h1>
+      <h2 onClick={changeIsMarried} style={{ fontSize: "130px" }}>
+        {isMarried ? "💑" : "🤼‍♂️"}
+      </h2>
+    </div>
+  );
 };
 
 export default Toggler;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import DrawerTask from "./99. Material-ui/drawerTask";
+import Toggler from "./100. todolist/toggler";
 class App extends Component {
   render() {
     return (
       <div>
-        <DrawerTask />
+        <Toggler />
       </div>
     );
   }
