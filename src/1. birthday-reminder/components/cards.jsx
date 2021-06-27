3a576029a5cd961ad68394d8d8d114edf072f8c6
Rafiur Rahman <rrfoundary@gmail.com>
2021-06-27 10:28:54 +0600

    BR - Refactor cards component

--- src/1. birthday-reminder/components/cards.jsx
+++ src/1. birthday-reminder/components/cards.jsx
@@ -1,35 +1,24 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import users from "../service/fakeUserGenerator";
 import Card from "./card";
 import "./styles/cards.css";
-class Cards extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: [],
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-  componentDidMount() {
-    this.setState({ users: [...users] });
-  }
-  handleClick() {
-    this.setState({ users: [] });
-  }
-  render() {
-    const { users } = this.state;
-    return (
-      <main className="cards">
-        <h3>{users.length} have birthday today</h3>
-        {users.map((user) => (
-          <Card key={user._id} user={user} />
-        ))}
-        <a onClick={this.handleClick} className="btn btn--primary">
-          Clear all
-        </a>
-      </main>
-    );
-  }
-}
+
+const Cards = (props) => {
+  const [users, setUsers] = useState(users);
+  const handleClick = () => {
+    setUsers({ users: [] });
+  };
+  return (
+    <main className="cards">
+      <h3>{users.length} have birthday today</h3>
+      {users.map((user) => (
+        <Card key={user._id} user={user} />
+      ))}
+      <a onClick={handleClick} className="btn btn--primary">
+        Clear all
+      </a>
+    </main>
+  );
+};
 
 export default Cards;
