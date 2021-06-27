bd7bd55a341e1213249f6402c369cdf7a980c7c2
Rafiur Rahman <rrfoundary@gmail.com>
2021-06-27 10:24:02 +0600

    Refactor card component to functional

--- src/1. birthday-reminder/components/card.jsx
+++ src/1. birthday-reminder/components/card.jsx
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import "./styles/card.css";
-class Card extends Component {
-  state = {};
-  render() {
-    const { user } = this.props;
-    return (
-      <div className="card">
-        <img src={user.img} alt="image name" className="card__img" />
-        <div className="card__text">
-          <h3 className="card__name">{user.name}</h3>
-          <p className="card__age">{user.age}</p>
-        </div>
+const Card = (props) => {
+  const { user } = props;
+
+  return (
+    <div className="card">
+      <img src={user.img} alt="image name" className="card__img" />
+      <div className="card__text">
+        <h3 className="card__name">{user.name}</h3>
+        <p className="card__age">{user.age}</p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Card;
