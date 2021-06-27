42d01a7d7c4990e86df484eabfccca59bd80416f
Rafiur Rahman <rrfoundary@gmail.com>
2021-06-27 09:49:32 +0600

    BR - Added card component

--- src/1. birthday-reminder/components/card.jsx
+++ src/1. birthday-reminder/components/card.jsx
@@ -2,7 +2,15 @@ import React, { Component } from "react";
 class Card extends Component {
   state = {};
   render() {
-    return <div className="card"></div>;
+    return (
+      <div className="card">
+        <img src="" alt="image name" className="card__img" />
+        <div className="card__text">
+          <h3 className="card__name">Usama</h3>
+          <p className="card__age">13</p>
+        </div>
+      </div>
+    );
   }
 }
 
