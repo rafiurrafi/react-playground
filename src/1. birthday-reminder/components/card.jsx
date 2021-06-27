ea5f385c5eb464fec67422fe30a6d66197d25e21
Rafiur Rahman <rrfoundary@gmail.com>
2021-06-27 09:43:06 +0600

    BR - Added fack server

---
+++ src/1. birthday-reminder/components/card.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+class Card extends Component {
+  state = {};
+  render() {
+    return <div className="card"></div>;
+  }
+}
+
+export default Card;
---
+++ src/1. birthday-reminder/service/fakeUserGenerator.js
@@ -0,0 +1,9 @@
+const users = [
+  { _id: 1, name: "Usama", age: 33, img: "img/08.jpg" },
+  { _id: 2, name: "Jainad", age: 33, img: "img/09.jpg" },
+  { _id: 3, name: "Abu farah", age: 34, img: "img/10.jpg" },
+  { _id: 4, name: "Usama", age: 40, img: "img/11.jpg" },
+  { _id: 5, name: "Lelith", age: 18, img: "img/14.jpg" },
+];
+
+export default users;
